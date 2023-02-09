c9d846024432eedaa569381b7279d3e708b3ef3a
Broderick <zerothesu@gmail.com>
2023-02-09 08:53:42 -0700

    add dotenv to server.js in order to get the connection to the .env file

--- server.js
+++ server.js
@@ -3,9 +3,12 @@ const express = require("express");
 const session = require("express-session");
 const exphbs = require("express-handlebars");
 const routes = require("./controllers");
+const dotenv = require('dotenv')
+dotenv.config()
 
 const sequelize = require("./config/connection");
 const SequelizeStore = require("connect-session-sequelize")(session.Store);
+
 
 const app = express();
 const PORT = process.env.PORT || 3008;
