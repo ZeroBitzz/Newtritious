a709e3820b4575f25b44dfd3fd30b61a7e1e9948
Broderick <zerothesu@gmail.com>
2023-02-15 04:54:44 -0700

    fix error with homepage crash

--- controllers/api/spoonacular.js
+++ controllers/api/spoonacular.js
@@ -2,34 +2,34 @@
 require('dotenv').config()
 
 exports.getRecipe = async function (cuisine, diet, tolerances, maxCalories) {
-    // console.log('spoonacular')
-    // const apiKey = process.env.API_KEY;
-    // const baseUrl = "https://api.spoonacular.com/recipes";
-    // let finalRecipe
-    // let randomRecipeId
-    // const parameters = {
-    //   apiKey: apiKey,
-    //   diet: diet,
-    //   cuisine: cuisine,
-    //   tolerances: tolerances,
-    //   calories: maxCalories,
-    // };
-    // console.log(apiKey)
+    console.log('-------------spoonacular-----------')
+    const apiKey = process.env.API_KEY;
+    const baseUrl = "https://api.spoonacular.com/recipes";
+    let finalRecipe
+    let randomRecipeId
+    const parameters = {
+      apiKey: apiKey,
+      diet: diet,
+      cuisine: cuisine,
+      tolerances: tolerances,
+      calories: maxCalories,
+    };
+    console.log(apiKey)
 
-    // let queryString = new URLSearchParams(parameters).toString();
-    // await fetch(`${baseUrl}/complexSearch?${queryString}`)
-    // .then(response => response.json())
-    // .then(data => {
-    //         randomRecipeId = data.results[Math.floor(Math.random() * data.results.length)].id;
-    // })
-    // await fetch(`${baseUrl}/${randomRecipeId}/information?apiKey=${apiKey}`)
-    // .then(response => response.json())
-    // .then(data => {
-    //     finalRecipe = data
-    // })
+    let queryString = new URLSearchParams(parameters).toString();
+    await fetch(`${baseUrl}/complexSearch?${queryString}`)
+    .then(response => response.json())
+    .then(data => {
+            randomRecipeId = data.results[Math.floor(Math.random() * data.results.length)].id;
+    })
+    await fetch(`${baseUrl}/${randomRecipeId}/information?apiKey=${apiKey}`)
+    .then(response => response.json())
+    .then(data => {
+        finalRecipe = data
+    })
     
 
-    // return finalRecipe
+    return finalRecipe
     // const randomRecipeId = data.results[Math.floor(Math.random() * data.results.length)].id;
     // const randomRecipeResponse = await fetch(`${recipeUrl}/?${randomRecipeId}/information`);
     // // to return all the data just type "return data"
