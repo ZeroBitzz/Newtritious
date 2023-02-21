8810e561d3b08d21dd168379b021da3f33ab234e
thenordplace <sadapan@gmail.com>
2023-02-20 19:26:24 -0700

    attempting to get login to work

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -14,7 +14,7 @@ router.get("/nothing", async (req, res) => {
 router.get("/login", (req, res) => {
   // If the user is already logged in, redirect the request to another route
   if (req.session.logged_in) {
-    res.redirect("/myrecipes");
+    res.redirect("/myRecipes");
     return;
   } else {
     res.render("login");
