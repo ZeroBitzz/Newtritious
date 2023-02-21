2116a2ad2428174cd40b3579ac8b51fdfb1a5237
thenordplace <sadapan@gmail.com>
2023-02-20 20:10:12 -0700

    attempting to get myRecipes to work

--- public/js/login.js
+++ public/js/login.js
@@ -16,7 +16,7 @@ const loginFormHandler = async (event) => {
     if (response.ok) {
       console.log("is it working?");
       // If successful, redirect the browser to the profile page
-      document.location.replace("/myRecipes");
+      document.location.replace("myRecipes/");
     } else {
       alert(response.statusText);
     }
