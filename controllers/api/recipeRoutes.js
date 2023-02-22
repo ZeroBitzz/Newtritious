30ae902c525187e9a572439ddf5794ca856ec3d4
thenordplace <sadapan@gmail.com>
2023-02-22 13:49:34 -0700

    registering allergies for fetch

--- controllers/api/recipeRoutes.js
+++ controllers/api/recipeRoutes.js
@@ -9,26 +9,32 @@ router.get("/", async (req, res) => {
   try {
     const userId = req.session.user_id;
     console.log(userId);
-    const userData = await User.findOne({
+    const userCuisine = await User.findOne({
       where: { id: userId },
       include: [{ model: UserCuisine }],
-      //   include: [{ model: UserAllergies }],
-      //   include: [{ model: UserDiet }],
     });
-    const usersCuisine = Object.keys(userData.userCuisine.dataValues).filter(
-      (k) => userData.userCuisine.dataValues[k] === true
+    const userAllergies = await User.findOne({
+      where: { id: userId },
+      include: [{ model: UserAllergies }],
+    });
+    // const userDiet = await User.findOne({
+    //   where: { id: userId },
+    //   include: [{ model: UserDiet }],
+    // });
+    const usersCuisine = Object.keys(userCuisine.userCuisine.dataValues).filter(
+      (k) => userCuisine.userCuisine.dataValues[k] === true
     );
-    // put these in if we get allergies and diet to work
-    // const usersAllergies = Object.keys(
-    //   userData.userAllergies.dataValues
-    // ).filter((k) => userData.userAllergies.dataValues[k] === true);
+    const usersAllergies = Object.keys(
+      userAllergies.userAllergy.dataValues
+    ).filter((k) => userAllergies.userAllergy.dataValues[k] === true);
+    // put this in if we get diet to work
     // const usersDiet = Object.keys(userData.userDiet.dataValues).filter(
     //   (k) => userData.userDiet.dataValues[k] === true
     // );
-    console.log(usersCuisine);
+    console.log(usersAllergies);
     const getApi = async (event) => {
       await fetch(
-        `https://api.spoonacular.com/recipes/complexSearch?apiKey=${apiKey}&cuisine=${usersCuisine}`,
+        `https://api.spoonacular.com/recipes/complexSearch?apiKey=${apiKey}&cuisine=${usersCuisine}&intolerances=${usersAllergies}`,
         {
           method: "POST",
           body: JSON.stringify(),
